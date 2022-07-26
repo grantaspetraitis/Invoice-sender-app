65b76920c3ac5379c802319ea4c726b03bfbe99a
grantaspetraitis <grantas157@gmail.com>
2022-07-26 15:50:02 +0300

    update to template

--- client/src/components/Teal.jsx
+++ client/src/components/Teal.jsx
@@ -4,7 +4,7 @@ const Teal = (props) => {
     return (
         <div className="preview">
             <h3 style={{ color: "#007655" }}>Sveiki, tėveliai,</h3>
-            <p>Siunčiu Jums sąskaitą ir prašau apmokėti už (<span className="italic">mėnesis</span>) mėn. būrelį (<span className="italic">{props.data.activity_title}</span>). Dėkoju, kad suteikiate savo vaikui galimybę lankyti (<span className="italic">būrelio pavadinimas</span>)</p>
+            <p>Siunčiu Jums sąskaitą ir prašau apmokėti už (<span className="italic">mėnesis</span>) mėn. būrelį (<span className="italic">{props.data[0].activity_title}</span>). Dėkoju, kad suteikiate savo vaikui galimybę lankyti (<span className="italic">būrelio pavadinimas</span>)</p>
             <p>Bankas: (<span className="italic">banko pavadinimas</span>)</p>
             <p>Gavėjas: (<span className="italic">gavėjo vardas</span>)</p>
             <p>Sąskaitos nr.: (<span className="italic">sąskaitos nr.</span>)</p>
